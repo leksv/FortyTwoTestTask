acca0b0be502470d73459d2fa2008e20c4cdd4ec
Алексей <aleks.woronow@yandex.ru>
2016-07-02 19:58:07 +0000

    add view hard data

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -5,4 +5,13 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    context = {
+        'name': 'Aleksey',
+        'surname': 'Voronov',
+        'date': '02.02.2016',
+        'bio': 'I was born ...',
+        'email': 'aleks.woronow@yandex.ru',
+        'jabber': 'leksw@42cc.co',
+        'skype': 'aleksey_woronov'
+    }
+    return render(request, 'home.html', context)
