c9d1bb75e6992d19d03f1774d67e64fc8e8c2b81
Алексей <aleks.woronow@yandex.ru>
2016-07-03 06:37:48 +0000

    ticket:1 deploy view that used model

--- apps/hello/tests/test_views.py
+++ apps/hello/tests/test_views.py
@@ -1,25 +1,46 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from datetime import date
+
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 
+from .. import models
+
 
 class HomePageTest(TestCase):
-    def test_home_page(self):
-        """
-        Test home page
-        """
+    def setUp(self):
+        models.Contact.objects.create(
+            name='Aleksey',
+            surname='Voronov',
+            email='aleks.woronow@yandex.ru',
+            date_of_birth=date(2016, 2, 25),
+            bio='I was born ...',
+            jabber='leksw@42cc.co',
+            skype_id='aleksey_woronov')
 
+    def test_home_page_response_and_template(self):
+        """
+        Test home page response and template.
+        """
         response = self.client.get(reverse('hello:home'))
 
         self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(response.status_code, 200)
         self.assertContains(response,
                             '<h1>42 Coffee Cups Test Assignment</h1>',
                             html=True)
-        self.assertContains(response, 'Aleks')
+
+    def test_home_page_one_contact(self):
+        """
+        Test home page if contact table has one record.
+        """
+        response = self.client.get(reverse('hello:home'))
+
+        self.assertContains(response, 'Aleksey')
         self.assertContains(response, 'Voronov')
-        self.assertContains(response, '02.02.2016')
+        self.assertContains(response, 'Feb. 25, 2016')
         self.assertContains(response, 'aleks.woronow@yandex.ru')
         self.assertContains(response, 'leksw@42cc.co')
         self.assertContains(response, 'aleksey_woronov')
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -3,15 +3,11 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from . import models
+
 
 def home_page(request):
-    context = {
-        'name': 'Aleksey',
-        'surname': 'Voronov',
-        'date': '02.02.2016',
-        'bio': 'I was born ...',
-        'email': 'aleks.woronow@yandex.ru',
-        'jabber': 'leksw@42cc.co',
-        'skype': 'aleksey_woronov'
-    }
+    context = {}
+    contact = models.Contact.objects.first()
+    context['contact'] = contact
     return render(request, 'home.html', context)
