524b7eac70ceb79953b764ba12451c12f2a08243
Алексей <aleks.woronow@yandex.ru>
2016-07-04 13:07:21 +0000

    added mockup to template

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.conf.urls import patterns, url
+from django.views.generic.base import TemplateView
 
 from .import views
 
@@ -9,4 +10,7 @@ from .import views
 urlpatterns = patterns(
     '',
     url(r'^$', views.home_page, name='home'),
+    url(r'^requests/$',
+        TemplateView.as_view(template_name="requests.html"),
+        name='requests'),
 )
