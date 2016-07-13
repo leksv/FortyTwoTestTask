aa5f8ba6a49a7d9485ff22829727b3795039fc81
Алексей <aleks.woronow@yandex.ru>
2016-07-13 15:54:54 +0000

    ticket:3 renamed middleware

--- fortytwo_test_task/settings/common.py
+++ fortytwo_test_task/settings/common.py
@@ -54,7 +54,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'apps.middleware.helloRequest.RequestMiddle',
+    'apps.middleware.helloRequest.RequestMiddleware',
 )
 
 ROOT_URLCONF = 'fortytwo_test_task.urls'
