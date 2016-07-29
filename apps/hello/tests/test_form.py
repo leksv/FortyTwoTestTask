41c11c583152f863eaaae5b7a71e317182109a09
Алексей <aleks.woronow@yandex.ru>
2016-07-29 17:02:36 +0000

    ticket:5 added date of birth validation

--- apps/hello/tests/test_form.py
+++ apps/hello/tests/test_form.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from datetime import date
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
 
 from django.test import TestCase
 
@@ -35,3 +37,35 @@ class FormTest(TestCase):
 
         form = ContactForm(data=form_data)
         self.assertEqual(form.is_valid(), True)
+
+    def test_form_data_of_birth_earlier_now(self):
+        """
+        Test that date of birth cannot be later then now or now.
+        """
+        delta = timedelta(days=2)
+        birth_date = date.today() + delta
+        form = ContactForm({'date_of_birth': birth_date})
+
+        self.assertEqual(form.is_valid(), False)
+        self.assertEqual(
+            form.errors['date_of_birth'],
+            ['Date of birth cannot be later then now or now.'])
+
+        form = ContactForm({'date_of_birth': date.today()})
+        self.assertEqual(form.is_valid(), False)
+        self.assertEqual(
+            form.errors['date_of_birth'],
+            ['Date of birth cannot be later then now or now.'])
+
+    def test_form_data_of_birth_later_then_100_years_ago(self):
+        """
+        Test that Date of birth cannot be earlier then 100 years ago.
+        """
+        delta = relativedelta(years=100)
+        birth_date = date.today() - delta
+        form = ContactForm({'date_of_birth': birth_date})
+
+        self.assertEqual(form.is_valid(), False)
+        self.assertEqual(
+            form.errors['date_of_birth'],
+            ['Date of birth cannot be later then now or now.'])
