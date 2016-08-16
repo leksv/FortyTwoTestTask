37d34ae377c91e97e9462f547a987835e4972bd9
Алексей <aleks.woronow@yandex.ru>
2016-08-16 07:36:04 +0000

    ticket:1 added logining to home_page

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,13 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import logging
+
 from django.shortcuts import render
+from django.http.response import HttpResponseServerError
 
 from hello.models import Contact
 
 
+logger = logging.getLogger("home_page")
+
+
 def home_page(request):
     context = {}
-    contact = Contact.objects.first()
+    contact = None
+
+    try:
+        contact = Contact.objects.first()
+    except Exception as err:
+        logger.error(err)
+        return HttpResponseServerError('Server Error (500)')
+
     context['contact'] = contact
     return render(request, 'home.html', context)
